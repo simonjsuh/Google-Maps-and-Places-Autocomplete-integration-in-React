5f24ebaab32dbeaad0c56ca9e3cc86f9cc240e57
simonjsuh <simon.j.suh@gmail.com>
2020-10-24 22:08:01 -0700

    tutorial source code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import GoogleMap from './components/GoogleMap';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Google Maps App</h1>
+      <GoogleMap />
+    </div>
+  );
+}
+
+export default App;
